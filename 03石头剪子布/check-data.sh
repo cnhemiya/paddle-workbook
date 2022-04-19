8b35a7f92b1c97a241756ea9450f9e06631252b5
cnhemiya <cnhemiya@qq.com>
2022-04-19 11:26:48 +0800

    增加 check-data.sh get-data.sh make-images-labels.py

---
+++ 03石头剪子布/check-data.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/bash
+
+# 检查 dataset 目录下的数据是否存在
+
+dataset_dir="./dataset"
+data_files="
+$dataset_dir/train-images-labels.txt
+$dataset_dir/test-images-labels.txt
+"
+data_dirs="
+$dataset_dir/rps-cv-images/rock
+$dataset_dir/rps-cv-images/scissors
+$dataset_dir/rps-cv-images/paper
+"
+
+# 检查文件
+check_files(){
+    paths=$@
+    for i in $paths
+    do
+        if [ -f "$i" ]; then
+            echo "检查文件: $i  --  存在"
+        else
+            echo "检查文件: $i  --  不存在"
+        fi
+    done
+}
+
+# 检查文件夹
+check_dirs(){
+    paths=$@
+    for i in $paths
+    do
+        if [ -d "$i" ]; then
+            echo "检查文件夹: $i  --  存在"
+        else
+            echo "检查文件夹: $i  --  不存在"
+        fi
+    done
+}
+
+check_files ${data_files[@]}
+check_dirs ${data_dirs[@]}
---
+++ 03石头剪子布/get-data.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/bash
+
+# 获取数据到 dataset 目录下
+
+dataset_dir="./dataset"
+ais_dataset_dir="../data/data75404"
+zip_files="rps-cv-images.zip"
+# data_files="train-images-labels.txt test-images-labels.txt"
+
+# 解压缩文件
+unzip_file(){
+    file="$1"
+    dir="$2"
+    ext="${file##*.}"
+    if [ -f "$file" ]; then
+        echo "解压文件: $file"
+        if [ $ext == "zip" ]; then
+            unzip -oq "$file" -d "$dir"
+        elif [ $ext == "gz" ]; then
+            gzip -dqkfN "$file"
+        fi
+    fi
+}
+
+# 获取数据
+get_data(){
+    file="$1"
+    if [ -f "$dataset_dir/$file" ]; then
+        echo "找到文件: $dataset_dir/$file"
+    elif [ -f "$ais_dataset_dir/$file" ]; then
+        echo "找到文件: $ais_dataset_dir/$file"
+        echo "移动文件到: $dataset_dir/$file"
+        mv "$ais_dataset_dir/$file" "$dataset_dir/$file"
+    fi
+    if [ "$2" == "zip" ]; then
+        unzip_file "$dataset_dir/$file" "$dataset_dir"
+    fi
+}
+
+# 获取全部压缩文件数据
+get_all_zip_data(){
+    for i in $zip_files
+    do
+        get_data "$i" "zip"
+    done
+}
+
+# 获取全部文件数据
+get_all_file_data(){
+    for i in $data_files
+    do
+        get_data "$i"
+    done
+}
+
+get_all_zip_data
+# get_all_file_data
