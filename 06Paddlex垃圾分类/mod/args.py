0406ebc67508e6d6a1436116a31a2ac9b45c9340
cnhemiya <cnhemiya@qq.com>
2022-05-17 12:48:03 +0800

    优化

--- 06Paddlex垃圾分类/mod/args.py
+++ 06Paddlex垃圾分类/mod/args.py
@@ -142,7 +142,10 @@ class TrainX():
         self.__label_list_path = label_list_path
         self.__save_dir_path = save_dir_path
 
-        self.args = self.parse() if args == None else args
+        self._arg_parse = argparse.ArgumentParser()
+        self.add_argument()
+        self.args = self._arg_parse.parse_args() if args == None else args
+
         self.cpu = self.args.cpu
         self.num_workers = "auto" if self.args.num_workers == 0 else self.args.num_workers
 
@@ -153,60 +156,57 @@ class TrainX():
         self.lr_decay_gamma = self.args.lr_decay_gamma
         self.save_interval_epochs = self.args.save_interval_epochs
         self.save_dir = save_dir_path if self.args.save_dir == "" else self.args.save_dir
-        self.dataset = dataset_path if self.args.dataset == "" else os.path.join(
-            dataset_path, self.args.dataset)
+        self.dataset = dataset_path if self.args.dataset == "" else self.args.dataset
         self.model = self.args.model
         self.pretrain_weights = self.args.pretrain_weights
         self.resume_checkpoint = self.args.resume_checkpoint
 
         self.model_list = self.args.model_list
-        self.train_list = os.path.join(self.dataset, self.args.train_list)
-        self.eval_list = os.path.join(self.dataset, self.args.eval_list)
-        self.label_list = os.path.join(self.dataset, self.args.label_list)
+        self.train_list = os.path.join(
+            self.dataset, train_list_path) if self.args.train_list == "" else self.args.train_list
+        self.eval_list = os.path.join(
+            self.dataset, eval_list_path) if self.args.eval_list == "" else self.args.eval_list
+        self.label_list = os.path.join(
+            self.dataset, label_list_path) if self.args.label_list == "" else self.args.label_list
 
-    def parse(self):
+    def add_argument(self):
         """
-        返回命令行参数
-
-        Returns:
-            argparse: 命令行参数
+        添加命令行参数
         """
-        arg_parse = argparse.ArgumentParser()
-        arg_parse.add_argument("--cpu", action="store_true",
-                               dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
-        arg_parse.add_argument("--num_workers", type=int, default=0,
-                               dest="num_workers", metavar="", help="线程数量，默认 auto，为CPU核数的一半")
-        arg_parse.add_argument("--epochs", type=int, default=4,
-                               dest="epochs", metavar="", help="训练几轮，默认 4 轮")
-        arg_parse.add_argument("--batch_size", type=int, default=16,
-                               dest="batch_size", metavar="", help="一批次数量，默认 16")
-        arg_parse.add_argument("--learning_rate", type=float, default=0.025,
-                               dest="learning_rate", metavar="", help="学习率，默认 0.025")
-        arg_parse.add_argument("--lr_decay_epochs", dest="lr_decay_epochs", default="30 60 90",
-                               metavar="", help="默认优化器的学习率衰减轮数。默认为 30 60 90")
-        arg_parse.add_argument("--lr_decay_gamma", type=float, default=0.1,
-                               dest="lr_decay_gamma", metavar="", help="默认优化器的学习率衰减率。默认为0.1")
-        arg_parse.add_argument("--save_interval_epochs", type=int, default=1,
-                               dest="save_interval_epochs", metavar="", help="模型保存间隔(单位: 迭代轮数)。默认为1")
-        arg_parse.add_argument("--save_dir", dest="save_dir", default="{}".format(self.__save_dir_path),
-                               metavar="", help="模型保存路径。默认为 {}".format(self.__save_dir_path))
-        arg_parse.add_argument("--dataset", dest="dataset", default="",
-                               metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
-        arg_parse.add_argument("--model", dest="model", default="",
-                               metavar="", help="PaddleX 模型名称")
-        arg_parse.add_argument("--pretrain_weights", dest="pretrain_weights", default="IMAGENET",
-                               metavar="", help="从文件加载模型权重，默认 IMAGENET 自动下载 ImageNet 预训练的模型权重")
-        arg_parse.add_argument("--resume_checkpoint", dest="resume_checkpoint", default="",
-                               metavar="", help="恢复训练时指定上次训练保存的模型路径, 默认不会恢复训练")
-        arg_parse.add_argument("--model_list", action="store_true", dest="model_list",
-                               help="输出 PaddleX 模型名称，默认不输出，选择后只输出信息，不会开启训练")
-        arg_parse.add_argument("--train_list", dest="train_list", default="{}".format(self.__train_list_path),
-                               metavar="", help="训练集列表，默认 {}".format(self.__train_list_path))
-        arg_parse.add_argument("--eval_list", dest="eval_list", default="{}".format(self.__eval_list_path),
-                               metavar="", help="评估集列表，默认 {}".format(self.__eval_list_path))
-        arg_parse.add_argument("--label_list", dest="label_list", default="{}".format(self.__label_list_path),
-                               metavar="", help="分类标签列表，默认{}".format(self.__label_list_path))
-        return arg_parse.parse_args()
+        self._arg_parse.add_argument("--cpu", action="store_true",
+                                     dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
+        self._arg_parse.add_argument("--num_workers", type=int, default=0,
+                                     dest="num_workers", metavar="", help="线程数量，默认 auto，为CPU核数的一半")
+        self._arg_parse.add_argument("--epochs", type=int, default=4,
+                                     dest="epochs", metavar="", help="训练几轮，默认 4 轮")
+        self._arg_parse.add_argument("--batch_size", type=int, default=16,
+                                     dest="batch_size", metavar="", help="一批次数量，默认 16")
+        self._arg_parse.add_argument("--learning_rate", type=float, default=0.025,
+                                     dest="learning_rate", metavar="", help="学习率，默认 0.025")
+        self._arg_parse.add_argument("--lr_decay_epochs", dest="lr_decay_epochs", default="30 60 90",
+                                     metavar="", help="默认优化器的学习率衰减轮数。默认为 30 60 90")
+        self._arg_parse.add_argument("--lr_decay_gamma", type=float, default=0.1,
+                                     dest="lr_decay_gamma", metavar="", help="默认优化器的学习率衰减率。默认为0.1")
+        self._arg_parse.add_argument("--save_interval_epochs", type=int, default=1,
+                                     dest="save_interval_epochs", metavar="", help="模型保存间隔(单位: 迭代轮数)。默认为1")
+        self._arg_parse.add_argument("--save_dir", dest="save_dir", default="{}".format(self.__save_dir_path),
+                                     metavar="", help="模型保存路径。默认为 {}".format(self.__save_dir_path))
+        self._arg_parse.add_argument("--dataset", dest="dataset", default="",
+                                     metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
+        self._arg_parse.add_argument("--model", dest="model", default="",
+                                     metavar="", help="PaddleX 模型名称")
+        self._arg_parse.add_argument("--pretrain_weights", dest="pretrain_weights", default="IMAGENET",
+                                     metavar="", help="从文件加载模型权重，默认 IMAGENET 自动下载 ImageNet 预训练的模型权重")
+        self._arg_parse.add_argument("--resume_checkpoint", dest="resume_checkpoint", default="",
+                                     metavar="", help="恢复训练时指定上次训练保存的模型路径, 默认不会恢复训练")
+        self._arg_parse.add_argument("--model_list", action="store_true", dest="model_list",
+                                     help="输出 PaddleX 模型名称，默认不输出，选择后只输出信息，不会开启训练")
+        self._arg_parse.add_argument("--train_list", dest="train_list", default="", metavar="",
+                                     help="训练集列表，默认 '--dataset' 参数目录下的 {}".format(self.__train_list_path))
+        self._arg_parse.add_argument("--eval_list", dest="eval_list", default="", metavar="",
+                                     help="评估集列表，默认 '--dataset' 参数目录下的 {}".format(self.__eval_list_path))
+        self._arg_parse.add_argument("--label_list", dest="label_list", default="", metavar="",
+                                     help="分类标签列表，默认 '--dataset' 参数目录下的 {}".format(self.__label_list_path))
 
     def check(self):
         mod.utils.check_path(self.dataset)
@@ -244,34 +244,32 @@ class TestX():
         self.__dataset_path = dataset_path
         self.__test_list_path = test_list_path
 
-        self.args = self.parse() if args == None else args
+        self._arg_parse = argparse.ArgumentParser()
+        self.add_argument()
+        self.args = self._arg_parse.parse_args() if args == None else args
+
         self.cpu = self.args.cpu
         self.epochs = self.args.epochs
 
-        self.dataset = dataset_path if self.args.dataset == "" else os.path.join(
-            dataset_path, self.args.dataset)
-        self.test_list = os.path.join(self.dataset, self.args.test_list)
+        self.dataset = dataset_path if self.args.dataset == "" else self.args.dataset
+        self.test_list = os.path.join(
+            self.dataset, test_list_path) if self.args.test_list == "" else self.args.test_list
         self.model_dir = self.args.model_dir
 
-    def parse(self):
+    def add_argument(self):
         """
-        返回命令行参数
-
-        Returns:
-            argparse: 命令行参数
+        添加命令行参数
         """
-        arg_parse = argparse.ArgumentParser()
-        arg_parse.add_argument("--cpu", action="store_true",
-                               dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
-        arg_parse.add_argument("--epochs", type=int, default=4,
-                               dest="epochs", metavar="", help="训练几轮，默认 4 轮")
-        arg_parse.add_argument("--dataset", dest="dataset", default="",
-                               metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
-        arg_parse.add_argument("--test_list", dest="test_list", default="{}".format(self.__test_list_path),
-                               metavar="", help="训练集列表，默认 {}".format(self.__test_list_path))
-        arg_parse.add_argument("--model_dir", dest="model_dir", default="./output/best_model",
-                               metavar="", help="读取训练后的模型目录，默认 ./output/best_model")
-        return arg_parse.parse_args()
+        self._arg_parse.add_argument("--cpu", action="store_true",
+                                     dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
+        self._arg_parse.add_argument("--epochs", type=int, default=4,
+                                     dest="epochs", metavar="", help="训练几轮，默认 4 轮")
+        self._arg_parse.add_argument("--dataset", dest="dataset", default="",
+                                     metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
+        self._arg_parse.add_argument("--test_list", dest="test_list", default="", metavar="",
+                                     help="训练集列表，默认 '--dataset' 参数目录下的 {}".format(self.__test_list_path))
+        self._arg_parse.add_argument("--model_dir", dest="model_dir", default="./output/best_model",
+                                     metavar="", help="读取训练后的模型目录，默认 ./output/best_model")
 
     def check(self):
         mod.utils.check_path(self.dataset)
@@ -289,40 +287,38 @@ class PredictX():
         self.__dataset_path = dataset_path
         self.__infer_list_path = infer_list_path
 
-        self.args = self.parse() if args == None else args
+        self._arg_parse = argparse.ArgumentParser()
+        self.add_argument()
+        self.args = self._arg_parse.parse_args() if args == None else args
+
         self.cpu = self.args.cpu
 
-        self.dataset = dataset_path if self.args.dataset == "" else os.path.join(
-            dataset_path, self.args.dataset)
-        self.infer_list = os.path.join(self.dataset, self.args.infer_list)
+        self.dataset = dataset_path if self.args.dataset == "" else self.args.dataset
+        self.infer_list = os.path.join(
+            self.dataset, infer_list_path) if self.args.infer_list == "" else self.args.infer_list
         self.model_dir = self.args.model_dir
         self.result_info = self.args.result_info
         self.result_path = self.args.result_path
         self.split = self.args.split
 
-    def parse(self):
+    def add_argument(self):
         """
-        返回命令行参数
-
-        Returns:
-            argparse: 命令行参数
+        添加命令行参数
         """
-        arg_parse = argparse.ArgumentParser()
-        arg_parse.add_argument("--cpu", action="store_true",
-                               dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
-        arg_parse.add_argument("--dataset", dest="dataset", default="",
-                               metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
-        arg_parse.add_argument("--infer_list", dest="infer_list", default="{}".format(self.__infer_list_path),
-                               metavar="", help="预测集列表，默认 {}".format(self.__infer_list_path))
-        arg_parse.add_argument("--model_dir", dest="model_dir", default="./output/best_model",
-                               metavar="", help="读取训练后的模型目录，默认 ./output/best_model")
-        arg_parse.add_argument("--result_info", action="store_true",
-                               dest="result_info", help="显示预测结果详细信息，默认 不显示")
-        arg_parse.add_argument("--result_path", dest="result_path", default="./result/result.csv",
-                               metavar="", help="预测结果文件路径，默认 ./result/result.csv")
-        arg_parse.add_argument("--split", dest="split", default=",",
-                               metavar="", help="数据分隔符，默认 ','")
-        return arg_parse.parse_args()
+        self._arg_parse.add_argument("--cpu", action="store_true",
+                                     dest="cpu", help="是否使用 cpu 计算，默认使用 CUDA")
+        self._arg_parse.add_argument("--dataset", dest="dataset", default="",
+                                     metavar="", help="数据集目录，默认 {}".format(self.__dataset_path))
+        self._arg_parse.add_argument("--infer_list", dest="infer_list", default="",
+                                     metavar="", help="预测集列表，默认 '--dataset' 参数目录下的 {}".format(self.__infer_list_path))
+        self._arg_parse.add_argument("--model_dir", dest="model_dir", default="./output/best_model",
+                                     metavar="", help="读取训练后的模型目录，默认 ./output/best_model")
+        self._arg_parse.add_argument("--result_info", action="store_true",
+                                     dest="result_info", help="显示预测结果详细信息，默认 不显示")
+        self._arg_parse.add_argument("--result_path", dest="result_path", default="./result/result.csv",
+                                     metavar="", help="预测结果文件路径，默认 ./result/result.csv")
+        self._arg_parse.add_argument("--split", dest="split", default=",",
+                                     metavar="", help="数据分隔符，默认 ','")
 
     def check(self):
         mod.utils.check_path(self.dataset)
