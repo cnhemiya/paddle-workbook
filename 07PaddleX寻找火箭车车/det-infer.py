1f439ae10b80393ee0d49136af905d5087f6a545
cnhemiya <cnhemiya@qq.com>
2022-05-23 22:23:43 +0800

    开发

--- 07PaddleX寻找火箭车车/det-infer.py
+++ 07PaddleX寻找火箭车车/det-infer.py
@@ -1,56 +1,109 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+"""
+LICENSE: MulanPSL2
+AUTHOR:  cnhemiya@qq.com
+DATE:    2022-05-23 21:29
+文档说明: 预测
+"""
 
 
+import os
+import sys
 import cv2
+import argparse
 import paddlex as pdx
 
 
-# 预测图像
-image_name = "./dataset/0084.jpg"
-# 读取模型
-model = pdx.load_model("./output/best_model")
-# score阈值，将Box置信度低于该阈值的框过滤
-threshold = 0.5
+def get_arg_parse():
+    arg_parse = argparse.ArgumentParser(
+        description="读取模型并预测")
+    arg_parse.add_argument("--model_dir", default="./output/best_model", dest="model_dir",
+                           metavar="", help="读取模型的目录，默认 './output/best_model'")
+    arg_parse.add_argument("--predict_image", default="", dest="predict_image",
+                           metavar="", help="预测的图像文件")
+    arg_parse.add_argument("--threshold", default=0.5, dest="threshold",
+                           metavar="", help="score阈值，将Box置信度低于该阈值的框过滤，默认 0.5")
+    arg_parse.add_argument("--result_list", default="./result/result.txt", dest="result_list",
+                           metavar="", help="预测的结果列表文件，默认 './result/result.txt'")
+    arg_parse.add_argument("--result_dir", default="./result", dest="result_dir",
+                           metavar="", help="预测结果可视化的保存目录，默认 './result'")
+    arg_parse.add_argument("--show_result", action="store_true", dest="show_result",
+                           help="显示预测结果的图像")
+    return arg_parse
 
-# 读取图像
-img = cv2.imread(image_name)
-# 预测结果
-result = model.predict(img)
-# 保留的结果
-keep_results = []
-# 面积
-areas = []
-# 写入文件的结果
-result_lines = []
-# 数量计数
-count = 0
 
-# 遍历结果，过滤
-for det in result:
-    cname, bbox, score = det["category"], det["bbox"], det["score"]
-    # 结果过滤
-    if score >= threshold:
-        count += 1
-        keep_results.append(det)
-        result_lines.append("{}\n".format(str(det)))
-        # 面积：宽 * 高
-        areas.append(bbox[2] * bbox[3]) 
+def infer(args):
+    # 读取模型
+    model = pdx.load_model(args.model_dir)
+    # 预测图像
+    predict_image = args.predict_image
+    # score阈值，将Box置信度低于该阈值的框过滤
+    threshold = args.threshold
+    # 预测的结果列表文件
+    result_list = args.result_list
+    # 预测结果可视化的保存目录
+    result_dir = args.result_dir
 
-# 面积降序排列
-# areas = np.asarray(areas)
-# sorted_idxs = np.argsort(-areas).tolist()
-# keep_results = [keep_results[k]
-#                 for k in sorted_idxs] if len(keep_results) > 0 else []
+    # 读取图像
+    img = cv2.imread(predict_image)
+    # 预测结果
+    result = model.predict(img)
+    # 保留的结果
+    keep_results = []
+    # 面积
+    areas = []
+    # 写入文件的结果
+    result_lines = []
+    # 数量计数
+    count = 0
 
-# 符合阈值 threshold 的结果数量
-total_str = "the total number is : {}".format(str(count))
-print(total_str)
+    # 遍历结果，过滤
+    for det in result:
+        cname, bbox, score = det["category"], det["bbox"], det["score"]
+        # 结果过滤
+        if score >= threshold:
+            count += 1
+            keep_results.append(det)
+            result_lines.append("{}\n".format(str(det)))
+            # 面积：宽 * 高
+            areas.append(bbox[2] * bbox[3])
 
-# 写入结果
-with open("./result/result.txt", "w") as f:
-    f.writelines(result_lines)
+    # 面积降序排列
+    # areas = np.asarray(areas)
+    # sorted_idxs = np.argsort(-areas).tolist()
+    # keep_results = [keep_results[k]
+    #                 for k in sorted_idxs] if len(keep_results) > 0 else []
 
-# 预测结果可视化
-pdx.det.visualize(
-    image_name, result, threshold=threshold, save_dir="./result")
+    # 符合阈值 threshold 的结果数量
+    total_str = "the total number is : {}".format(str(count))
+    print(total_str)
+
+    # 写入结果
+    with open(result_list, "w") as f:
+        f.writelines(result_lines)
+
+    # 预测结果可视化
+    pdx.det.visualize(
+        predict_image, result, threshold=threshold, save_dir=result_dir)
+
+    # 显示预测结果的图像
+    if args.show_result:
+        image_path = os.path.join(result_dir, "visualize_{}".format(
+            os.path.basename(predict_image)))
+        img = cv2.imread(image_path)
+        cv2.imshow("result", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
+
+def main():
+    arg_parse = get_arg_parse()
+    if (len(sys.argv) < 2):
+        arg_parse.print_help()
+    else:
+        infer(arg_parse.parse_args())
+
+
+if __name__ == "__main__":
+    main()
